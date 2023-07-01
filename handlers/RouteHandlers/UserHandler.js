ea944cc0efe376c396d671c8e3bc9dd782bd770a
tamimhasan19702 <tamimhasancu@gmail.com>
2023-07-01 18:16:21 +0600

    handle user delete successfully

--- handlers/RouteHandlers/UserHandler.js
+++ handlers/RouteHandlers/UserHandler.js
@@ -198,6 +198,42 @@ handler._users.put = (requestProperties, callback) => {
   }
 };
 
-handler._users.delete = (requestProperties, callback) => {};
+// deleted user information with this handler
+
+handler._users.delete = (requestProperties, callback) => {
+  //check the phone number is valid or not
+  const phone =
+    typeof requestProperties.queryStringObject.phone === "string" &&
+    requestProperties.queryStringObject.phone.trim().length === 11
+      ? requestProperties.queryStringObject.phone
+      : null;
+
+  if (phone) {
+    //lookup the user
+    data.read("users", phone, (err1, userData) => {
+      if (!err1 && userData) {
+        data.delete("users", phone, (err2) => {
+          if (!err2) {
+            callback(200, {
+              message: "User was successfully deleted!",
+            });
+          } else {
+            callback(500, {
+              error: "There was a server side error",
+            });
+          }
+        });
+      } else {
+        callback(500, {
+          error: "There was a server side error!",
+        });
+      }
+    });
+  } else {
+    callback(400, {
+      error: "There was a problem in your request",
+    });
+  }
+};
 
 module.exports = handler;
