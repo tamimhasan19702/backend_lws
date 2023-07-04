3dd139c9f71fbd6aa67c88f81a90ffa54d9cb070
tamimhasan19702 <tamimhasancu@gmail.com>
2023-07-04 18:07:12 +0600

    randomString function created

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -2,10 +2,13 @@
  * title: utilities
  * description: Important utility functions
  * Author : tareq
+ *
+ * @format
  */
+
 //dependencies
-const crypto = require('crypto');
-const environments = require('./environments');
+const crypto = require("crypto");
+const environments = require("./environments");
 
 //module scaffholding
 
@@ -13,35 +16,46 @@ const utilities = {};
 
 //parse JSON string to object
 utilities.parseJSON = (jsonString) => {
-    let output ;
+  let output;
 
-    try{
-     output = JSON.parse(jsonString);
-    }catch{
-      output = {};
-    }
+  try {
+    output = JSON.parse(jsonString);
+  } catch {
+    output = {};
+  }
 
-    return output;
-}
+  return output;
+};
 
 //hashing the user password
 utilities.hash = (str) => {
-    if(typeof(str) === 'string' && str.length > 0){
-      let hash = crypto
-      .createHmac('SHA256', environments.secretKey)
+  if (typeof str === "string" && str.length > 0) {
+    let hash = crypto
+      .createHmac("SHA256", environments.secretKey)
       .update(str)
-      .digest('hex');
-      return hash;
-    }
-    return false;
-}
-
+      .digest("hex");
+    return hash;
+  }
+  return false;
+};
 
 //create random string
 utilities.createRandomString = (strlength) => {
- 
-  return 'ahsaahjsha';
-}
+  let length = strlength;
+  length = typeof strlength === "number" && strlength > 0 ? strlength : false;
+
+  if (length) {
+    let possibleCharacters = "abcdefghijklmnopqrstuvwxyz1234567890";
+    let output = "";
+    for (let i = 1; i <= length; i + 1) {
+      let randomCharacter = possibleCharacters.charAt(
+        Math.floor(Math.random() * possibleCharacters.length)
+      );
+    }
+    output += randomCharacter;
+  }
+  return false;
+};
 
 // export module
 module.exports = utilities;
