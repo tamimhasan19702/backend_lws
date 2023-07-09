fe998ff32e215bc750d3a7e469b0cc4738a9fa4d
tamimhasan19702 <tamimhasancu@gmail.com>
2023-07-09 20:09:27 +0600

    validation of inputs in handler check post done

--- handlers/RouteHandlers/checkHandler.js
+++ handlers/RouteHandlers/checkHandler.js
@@ -27,25 +27,49 @@ handler.checkHandler = (requestProperties, callback) => {
 //extra scaffholding for checks
 handler._check = {};
 
-
 //post method used to handle new check
 handler._check.post = (requestProperties, callback) => {
-  
+  //validate inputs
+
+  let protocol =
+    typeof requestProperties.body.protocol === "string" &&
+    ["http", "https"].indexOf(requestProperties.body.protocol) > -1
+      ? requestProperties.body.protocol
+      : false;
+
+  let url =
+    typeof requestProperties.body.url === "string" &&
+    requestProperties.body.url.trim().length > 0
+      ? requestProperties.body.url
+      : false;
+
+  let method =
+    typeof requestProperties.body.method === "string" &&
+    ["get", "post", "put", "delete"].indexOf(requestProperties.body.method) > -1
+      ? requestProperties.body.method
+      : false;
+
+  let successCodes =
+    typeof requestProperties.body.successCodes === "object" &&
+    requestProperties.body.successCodes instanceof Array
+      ? requestProperties.body.successCodes
+      : false;
+
+  let timeOutSeconds =
+    typeof requestProperties.body.timeOutSeconds === "number" &&
+    requestProperties.body.timeOutSeconds % 1 === 0 &&
+    requestProperties.body.timeOutSeconds >= 1 &&  requestProperties.body.timeOutSeconds <= 5
+      ? requestProperties.body.successCodes
+      : false;
 };
 
 // give response to check handler function
-handler._check.get = (requestProperties, callback) => {
- 
-};
+handler._check.get = (requestProperties, callback) => {};
 
 //update the existing check handler response
-handler._check.put = (requestProperties, callback) => {
-  
-};
+handler._check.put = (requestProperties, callback) => {};
 
 // deleted the existing check request
-handler._check.delete = (requestProperties, callback) => {
-  
-};
+handler._check.delete = (requestProperties, callback) => {};
 
 module.exports = handler;
