ec143af88c4a4ef30cafd9ec20b7fa3fe1e7aa53
tamimhasan19702 <tamimhasancu@gmail.com>
2023-07-10 20:03:43 +0600

    notification.js helper function created successfully

--- helpers/environments.js
+++ helpers/environments.js
@@ -1,37 +1,49 @@
 /**
  * title: Environments
  * description: handels all the environment related things
+ *
+ * @format
  */
 
 //module scaffholding
 
-const environments = {}
+const environments = {};
 
 //staging environment
 environments.staging = {
-    port: 5000,
-    envName: 'staging',
-    secretKey: 'TamimHasan',
-    maxChecks: 5,
-}
+  port: 5000,
+  envName: "staging",
+  secretKey: "TamimHasan",
+  maxChecks: 5,
+  twilio: {
+    formPhone: "+8801714270830",
+    accountSid: "AC16fe2942c78aef53a3a5323beb8b86f7",
+    authToken: "bb977b08e3bcdabf3427b961ba1fd2f8",
+  },
+};
 
 //production environment
 environments.production = {
-    port : 6000,
-    envName: 'production',
-    secretKey: 'TareqMonower',
-    maxChecks: 5,
-}
+  port: 6000,
+  envName: "production",
+  secretKey: "TareqMonower",
+  maxChecks: 5,
+  twilio: {
+    formPhone: "+8801714270830",
+    accountSid: "AC16fe2942c78aef53a3a5323beb8b86f7",
+    authToken: "bb977b08e3bcdabf3427b961ba1fd2f8",
+  },
+};
 
 // determine which environment was passed
 const currentEnvironment =
-    typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV : 'staging';
+  typeof process.env.NODE_ENV === "string" ? process.env.NODE_ENV : "staging";
 
 // export corresponding environment object
 const environmentToExport =
-    typeof environments[currentEnvironment] === 'object'
-        ? environments[currentEnvironment]
-        : environments.staging;
+  typeof environments[currentEnvironment] === "object"
+    ? environments[currentEnvironment]
+    : environments.staging;
 
 // export module
 module.exports = environmentToExport;
