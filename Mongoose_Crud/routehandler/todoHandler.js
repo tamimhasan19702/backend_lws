8235f2a465f8c84d9044a6521c9c6d26fb25765a
tamimhasan19702 <tamimhasancu@gmail.com>
2023-08-22 22:19:10 +0600

    update todoHandler.js

--- Mongoose_Crud/routehandler/todoHandler.js
+++ Mongoose_Crud/routehandler/todoHandler.js
@@ -1,7 +1,10 @@
 /** @format */
 
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+const todoSchema = require("./todoSchema");
+const Todo = new mongoose.model("Todo", todoSchema);
 
 //GET ALL THE TODO
 router.get("/", async (req, res) => {});
@@ -10,13 +13,55 @@ router.get("/", async (req, res) => {});
 router.get("/:id", async (req, res) => {});
 
 //POST TODO
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+    const newTodo = new Todo(req.body)
+    await newTodo.save((err) => {
+        if(!err){
+            res.status(200).json({
+                message: "Todo Created Successfully"
+            })
+        }else{
+            res.status(500).json({
+                error: 'There was a server side error!!'
+            })
+        }
+    });
+});
 
-//POST MULTIPLE TODO
-router.post("/all", async (req, res) => {});
+//POST or update MULTIPLE TODO
+router.post("/all", async (req, res) => {
+    await Todo.insertMany(req.body, (err) => {
+        if(err){
+            res.status(500).json({
+                error: "There was a server side error!!"
+            })
+        } else{
+            res.status(200).json({
+                message: "Todos Created Successfully"
+            })
+        }
+    })
+});
 
 //put Todo
-router.put("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+    await Todo.updateOne({_id: req.params.id}, {
+        $set: {
+            status: 'active'
+        }
+    }, (err) => {
+        if(err){
+            res.status(500).json({
+                error: "There was a server side error!!"
+            })
+        } else{
+            res.status(200).json({
+                message: "Todo Updated Successfully"
+            })
+        }
+    });
+});
+
 
 //delete todo
 router.delete("/:id", async (req, res) => {});
